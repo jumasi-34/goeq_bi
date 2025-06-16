1d5116f235aa8c14758485b554d41b036e66ce93
jumasi-34 <kuac34@gmail.com>
2025-06-16 21:16:47 +0900

    업데이트

--- _01_query/GMES/q_rr.py
+++ _01_query/GMES/q_rr.py
@@ -196,7 +196,7 @@ def rr_oe_list() -> str:
 
 
 # ISO Local to HK Global 보정 계수 데이터 로드
-PATH = "_01_query/CSV/iso_local_to_hkGlobal.csv"
+PATH = os.path.join(project_root, "_01_query/CSV/iso_local_to_hkGlobal.csv")
 rr_corr_csv = pd.read_csv(PATH)
 
 
